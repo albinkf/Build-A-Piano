7c1c02e4ddc4048a21628b0bec566a24f56df079
Katie A <ktfalbin@gmail.com>
2026-01-17 18:37:51 -0500

    changed keys

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
-const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+const WHITE_KEYS = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
+const BLACK_KEYS = ['w', 'e', 't', 'y', 'u'];
 
 const whiteKeys = document.querySelectorAll('.key.white');
 const blackKeys = document.querySelectorAll('.key.black');

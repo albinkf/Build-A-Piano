2a6455bd287dbfb4d31f6cfef831da15dd28f59a
Katie A <ktfalbin@gmail.com>
2026-01-17 18:29:36 -0500

    adding keyboard functionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
+
+
+
 const keys = document.querySelectorAll('.key');
 //^ looks for everything with the class of key and classifies it as key//
 
@@ -30,3 +39,19 @@ function playNote(key) {
     key.classList.remove('active');
   });
 }
+//--EVENT LISTENERS FOR KEYBOARD--
+
+document.addEventListener('keydown', (e) => {
+  // Get the key that triggered the event
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+  if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+});

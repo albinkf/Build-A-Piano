712be0ed7119e34d7d78783a20ff389bbd421db3
Katie A <ktfalbin@gmail.com>
2026-01-17 18:19:00 -0500

    added active class

--- script.js
+++ script.js
@@ -13,10 +13,20 @@ keys.forEach((key) => {
 //function that plays the note when a key is clicked//
 
 function playNote(key) {
-    //selects note element//
-    //get key, go into data, look at note//
 
+    //selects note element//
+    
+    //get key, go into data, look at note//
   const noteAudio = document.getElementById(key.dataset.note);
- 
+  //reset note audio to 0//
+  noteAudio.currentTime = 0;
+  //play note audio//
   noteAudio.play();
+  //add class of active, to style//
+    key.classList.add('active');
+    //when sound ends, removes class of active //
+    noteAudio.addEventListener('ended', () => {
+        //access key and remove active//
+    key.classList.remove('active');
+  });
 }
